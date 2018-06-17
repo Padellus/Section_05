7ee3adacb799134bb5e5fcdca5f8b62eb9de8c74
Padellus <philepatti@gmail.com>
2018-06-17 02:28:10 -0400

    remove old patrolling guard class. material instances

--- Source/TestingGrounds/Private/ChooseNextWaypoint.cpp
+++ Source/TestingGrounds/Private/ChooseNextWaypoint.cpp
@@ -1,7 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "ChooseNextWaypoint.h"
-#include "PatrollingGuard.h" // TODO remove coupling
 #include "AIController.h"
 #include "PatrolRoute.h"
 #include "BehaviorTree/BehaviorTreeTypes.h"
